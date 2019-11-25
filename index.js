87e1ce324da9a1d53e80fd475db172cab434290e
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2019-11-25 14:08:28 -0500

    Comprobando los CORS

--- index.js
+++ index.js
@@ -10,23 +10,21 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 
 // app.use(cors());
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
-
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({
+            ok: true
+        })
+    }
+});
 
 /* User Routes */
 app.use('/api/registerUser', require('./routes/api/user/registerUser'));
