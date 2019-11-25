4d6ce5aa731d4ec6804c85e00986c68f2c78832c
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2019-11-25 11:28:44 -0500

    Cors en el index

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
-// app.use(cors());
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
